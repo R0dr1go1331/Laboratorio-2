09761ebd676398a6cfe3246bb30e1b71c6732180
rodrigo2003 <rs0212032021@unab.edu.sv>
2021-10-12 10:41:15 -0600

    version1
    primer

---
+++ Laboratorio2/Laboratorio2/Repository/ClsRPersona.cs
@@ -0,0 +1,36 @@
+﻿using Laboratorio2.Data;
+using Laboratorio2.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2.Repository
+{
+    class ClsRPersona : Ipersona
+    {
+        public void Create(persona Per)
+        {
+            using (almacendepersonasEntities almacendepersonas = new almacendepersonasEntities())
+            {
+                persona persona = new persona();
+                persona.nombrePersona = Per.nombrePersona;
+
+                persona.edadPersona = Per.edadPersona;
+                persona.descripcionPersona = Per.descripcionPersona;
+                almacendepersonas.persona.Add(persona);
+                almacendepersonas.SaveChanges();
+            }
+        }
+
+        public List<persona> PersonaLista()
+        {
+            using (almacendepersonasEntities almacendepersonas = new almacendepersonasEntities())
+            {
+                List<persona> Lpersonas = almacendepersonas.persona.ToList();
+                return Lpersonas;
+            }
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio2/Service/Ipersona.cs
@@ -0,0 +1,18 @@
+﻿using Laboratorio2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2.Service
+{
+    interface Ipersona
+    {
+        void Create(persona Per);
+         List<persona> PersonaLista();
+
+
+
+    }
+}
